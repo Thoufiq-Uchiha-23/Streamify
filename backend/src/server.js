be20dec503aa29bb9a71403efdaf951badbd6e9c
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-07-05 10:08:58 +0530

    server file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT
 
-app.get("/api/auth/signup", (req, res) => {
-  res.send("Signup Route");
-});
+app.use("/api/auth", authRoutes)
 
-app.get("/api/auth/login", (req, res) => {
-  res.send("Login Route");
-});
-
-app.get("/api/auth/logout", (req, res) => {
-  res.send("Logout Route");
-});
 app.listen(PORT, () => {
   console.log("Server is running on port:", PORT);
+  connectDB();
 });
