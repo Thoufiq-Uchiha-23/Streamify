65edb71256f32cb6c5d514eeaa24433e0c328a41
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-07-11 22:26:49 +0530

    minor change

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -42,6 +42,11 @@ export async function signup(req, res) {
     const token = jwt.sign({userId: newUser._id}, process.env.JWT_SECRET_KEY, {
       expiresIn: "7d"
     })
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      httpOnly: true,
+    })
   } catch (error) {}
 }
 
