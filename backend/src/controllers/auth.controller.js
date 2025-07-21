49c696231e35cb6ad5a1f266368fa5b7ecc1650d
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-07-21 11:01:13 +0530

    adding login and logout with signup check

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { upsertStreamUser } from "../lib/stream.js";
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 
@@ -21,48 +22,95 @@ export async function signup(req, res) {
       return res.status(400).json({ message: "Invalid email format" });
     }
 
-    const existingUser = await User.findOne({email})
+    const existingUser = await User.findOne({ email });
 
-    if(existingUser) {
-      return res.status(400).json({message: "Email already exists. please use a different one"})
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Email already exists. please use a different one" });
     }
 
     const idx = Math.floor(Math.random() * 100) + 1; // generate a number between 1-100
-    const randomAvatar = `https://avatar-placeholder.iran.liara.run/public/${idx}.png`
+    const randomAvatar = `https://avatar-placeholder.iran.liara.run/public/${idx}.png`;
 
     const newUser = await User.create({
       email,
       password,
       fullName,
-      profilePic : randomAvatar
-    })
+      profilePic: randomAvatar,
+    });
 
     //jwt
 
-    // TODO: CREATE THE USER IN STREAM AS WELL
+    try {
+      await upsertStreamUser({
+        id: newUser._id.toString(),
+        name: newUser.fullName,
+        image: newUser.profilePic || "",
+      });
+      console.log(`Stream user created for ${newUser.fullName}`)
+    } catch (error) {
+      console.error("Error creating Stream user:", error);
+    }
 
-    const token = jwt.sign({userId: newUser._id}, process.env.JWT_SECRET_KEY, {
-      expiresIn: "7d"
-    })
+    const token = jwt.sign(
+      { userId: newUser._id },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "7d",
+      }
+    );
 
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       httpOnly: true, // prevent XSS attacks
       sameSite: "strict", // prevent CSRF attacks
-      secure: process.env.NODE_ENV === "production"
-    })
+      secure: process.env.NODE_ENV === "production",
+    });
 
-    res.status(201).json({success: true, user: newUser})
+    res.status(201).json({ success: true, user: newUser });
   } catch (error) {
-    console.log("Error in signup controller:", error)
-    res.status(500).json({message: "Internal Server Error"})
+    console.log("Error in signup controller:", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 }
 
 export async function login(req, res) {
-  res.send("Login Route");
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(401).json({ message: "Invalid email or password" });
+
+    const isPasswordCorrect = await user.matchPassword(password);
+    if (!isPasswordCorrect)
+      return res.status(401).json({ message: "Invalid email or password" });
+
+    // create token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      httpOnly: true, // prevent XSS attacks
+      sameSite: "strict", // prevent CSRF attacks
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
 export async function logout(req, res) {
-  res.send("Logout Route");
+  res.clearCookie("jwt");
+  res.status(200).json({ success: true, message: "Logout successful" });
 }
