3c28f6bba52b20ae86cc72baf65637f231123d3b
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-08-04 10:26:48 +0530

    new routes for friends

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {protectRoute} from "../middleware/auth.middleware.js";
-import { getMyFriends, getRecommendedUsers } from "../controllers/user.controller.js";
+import { acceptFriendRequest, getFriendRequests, getMyFriends, getRecommendedUsers } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
@@ -11,5 +11,8 @@ router.get("/", getRecommendedUsers)
 router.get("/friends", getMyFriends)
 
 router.post("/friend-request/:id", sendFriendRequest);
+router.put("/friend-request/:id/accept", acceptFriendRequest);
+
+router.get("/friend-requests", getFriendRequests);
 
 export default router;
