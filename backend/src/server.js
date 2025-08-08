1b21a6ddd058740e80ea624ca44b4e75d99356fe
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-08-08 09:26:20 +0530

    cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import authRoutes from "./routes/auth.route.js"
-import userRoutes from "./routes/user.route.js"
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
 
 import { connectDB } from "./lib/db.js";
@@ -11,14 +12,20 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.use(express.json())
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // allow fronted to send cookies
+  })
+);
+app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
-app.use("/api/users", userRoutes)
-app.use("/api/chat", chatRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port:", PORT);
