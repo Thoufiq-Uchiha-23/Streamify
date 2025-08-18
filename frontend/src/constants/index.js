24706ba685c79c5cd3aa5b997b6b1785343a2999
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-08-18 07:59:30 +0530

    added in constants flag

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -195,3 +195,19 @@ export const LANGUAGE_TO_FLAG = {
   turkish: "tr",
   dutch: "nl",
 };
+
+// export const getLanguageFlag = (language) => {
+//   if (!language) return null;
+
+//   const langLower = language.toLowerCase();
+//   const countryCode = LANGUAGE_TO_FLAG[langLower];
+
+//   if (countryCode) {
+//     return (
+//       // <div>
+//         <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt={`${langLower} flag`} className="h-3 mr-1 inline-block" />
+//       // </div>
+//     );
+//   }
+//   return null;
+// };
