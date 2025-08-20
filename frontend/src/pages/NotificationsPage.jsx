5825ceef195ce5470b27e0891397f7d3abac5892
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-08-20 09:11:07 +0530

    notifications page completed

--- frontend/src/pages/NotificationsPage.jsx
+++ frontend/src/pages/NotificationsPage.jsx
@@ -2,6 +2,7 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import React from "react";
 import { acceptFriendRequest, getFriendRequests } from "../lib/api";
 import { BellIcon, ClockIcon, MessageSquareIcon, UserCheckIcon } from "lucide-react";
+import NoNotificationsFound from "../components/NoNotificationsFound";
 
 const NotificationsPage = () => {
   const queryClient = useQueryClient();
