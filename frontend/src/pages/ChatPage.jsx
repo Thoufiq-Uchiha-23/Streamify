6706c4631a0c202a7c84aed306b1c5f88d076068
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-08-20 09:10:47 +0530

    chat page building

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useParams } from "react-router";
+import useAuthUser from "../hooks/useAuthUser";
+import { useQueries, useQuery } from "@tanstack/react-query";
+import { getStreamToken } from "../lib/api";
+
+import {
+  Channel,
+  ChannelHeader,
+  Chat,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from "stream-chat-react";
 
 const ChatPage = () => {
-  return (
-    <div>
-      Chat Page
-    </div>
-  )
-}
+  const { id: targetUserId } = useParams();
 
-export default ChatPage
+  const [chatClient, setChatClient] = useState(null);
+  const [channel, setChannel] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const { authUser } = useAuthUser();
+
+  const { data } = useQuery({
+    queryKey: ["streamToken"],
+    queryFn: getStreamToken,
+    enabled: !!authUser // this will run only when authUser is available
+  });
+  return <div>Chat Page</div>;
+};
+
+export default ChatPage;
